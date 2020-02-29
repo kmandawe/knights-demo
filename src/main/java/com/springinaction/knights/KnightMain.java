9b9dae74c8f3c70af2a34fa2a21e4b76374f4590
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-02-29 18:29:35 +0800

    1.5 KnightMain - Application Context loading

--- src/main/java/com/springinaction/knights/KnightMain.java
+++ src/main/java/com/springinaction/knights/KnightMain.java
@@ -5,11 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
   public static void main(String[] args) {
-    ApplicationContext context = 
-        new ClassPathXmlApplicationContext("knights.xml");
-    
+    ApplicationContext context = new ClassPathXmlApplicationContext("knights.xml");
+
     Knight knight = (Knight) context.getBean("knight");
-    
+
     knight.embarkOnQuest();
   }
 }
